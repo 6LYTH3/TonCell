20d703fd1a66b7921b407a8ff47dc835b00c5515
Naruedol Yoojun <naruedol.yoojun@seagate.com>
2017-03-24 11:23:23 +0700

    Hacking

---
+++ renderer.js
@@ -0,0 +1,50 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+const fs = require('fs');
+
+var app = angular.module('mainApp', []);
+app.controller('MainCtr', ['$scope', '$http', function ($scope, $http) {
+    $scope.Boom = function () {
+        if (typeof $scope.paramX !== 'undefined' && typeof $scope.paramY !== 'undefined') {
+            console.log($scope.paramX + " : " + $scope.paramY);
+            Readfile();
+        } else {
+            console.log("Hacking");
+        }
+    };
+
+    var Readfile = function () {
+        fs.readFile('./abc.mf', (err, data) => {
+            if (err) throw err;
+            FindCell(data.toString());
+        });
+    }
+
+    var FindCell = function (data) {
+        var input = data.split("\n");
+        var output = "";
+        var pattern = /^CELL/g;
+
+        input.forEach(function (v) {
+            var reArray = pattern.exec(v);
+            if(reArray != null) {
+                output += ReplaceX(reArray.input) + "\n";
+            } else {
+                output += v + "\n";
+            }
+
+        }, this);
+        console.log(output);
+    }
+
+    var ReplaceX = function(data) {
+        var input = data.split(",");
+        var output = "";
+        input[4] = $scope.paramX;
+        input.forEach(function (v) {
+            output += v + ",";
+        }, this);
+        return output;
+    }
+}]);
